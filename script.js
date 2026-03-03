09b722507fc2882bd7cbbfb23d3a898a1a8c66d7
ErizoArbolito <erizoarbolito@gmail.com>
2026-03-03 18:49:59 +0000

    nnn

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 const titulo = document.getElementById("titulo");
 const texto = document.getElementById("texto")
 
-
 let textoInicial = true; //(variable tipo booleano - dos valores posiblws)
 let iTexto = 0;
 
-const textos = ["uno", "dos", "tres", "cuatro", "cinco"];
+const textos = ["Para iniciar", "tienes que", "hacer", "click allá", "↖"];
 console.log(textos[0]);
 
-titulo.addEventListener("mousemove", () => {
+titulo.addEventListener("click", () => {
 
     if(textoInicial === true) {
         titulo.innerText = "Qué visaje";;
